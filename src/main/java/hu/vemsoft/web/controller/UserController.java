a3ddca83d106028a09c560278669c45b829f40b4
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-04-28 22:40:25 +0200

    setup dispatcher servlet and create first controller

--- src/main/java/hu/vemsoft/web/controller/UserController.java
+++ src/main/java/hu/vemsoft/web/controller/UserController.java
@@ -1,4 +1,15 @@
 package hu.vemsoft.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    @GetMapping("profile")
+    public String profile() {
+        // we tell the controller where to look for the desired view file
+        // the exact same view can be requested via the browser by explicitly putting /jsp/profile.jsp into the URL
+        return "jsp/profile.jsp";
+    }
+
 }
